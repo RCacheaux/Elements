929c74719ee6eda21ac802a09c143873b835ac7c
René Cacheaux <rcacheaux@atlassian.com>
2016-02-03 21:10:27 -0600

    Initial commit.

---
+++ Elements/Elements/Elements.h
@@ -0,0 +1,19 @@
+//
+//  Elements.h
+//  Elements
+//
+//  Created by Rene Cacheaux on 2/3/16.
+//  Copyright © 2016 RCacheaux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Elements.
+FOUNDATION_EXPORT double ElementsVersionNumber;
+
+//! Project version string for Elements.
+FOUNDATION_EXPORT const unsigned char ElementsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Elements/PublicHeader.h>
+
+
